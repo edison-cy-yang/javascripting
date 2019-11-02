5a9689b7917de45239218e38bc22d71da8270f54
Edison Yang <edison.cy.yang@gmail.com>
2019-11-02 18:54:54 +0000

    Initial commit (2)

---
+++ if-statement.js
@@ -0,0 +1,8 @@
+let fruit;
+fruit = 'oragne';
+if(fruit.length > 5) {
+  console.log('The fruit name has more than five characters.');
+}
+else {
+  console.log('The fruit name has give characters or less');
+}
